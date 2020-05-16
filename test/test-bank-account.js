5e9bb35e899eeb012afa53e071a1166e39901eae
Alonso <a01650552@itesm.mx>
2020-05-15 19:34:42 -0500

    probar el restar del balance final al combinar cuentas

--- test/test-bank-account.js
+++ test/test-bank-account.js
@@ -7,7 +7,7 @@ describe('BankAccount', () => {
             //returning current balance
             let balance = 0;
             let account = new BankAccount(balance);
-            assert.deepEqual(balance, account.current());
+            assert.equal(balance, account.current());
         })
     })
 
@@ -17,12 +17,12 @@ describe('BankAccount', () => {
             let balance = 10;
             let amount = 50;
             let account = new BankAccount(balance);
-            assert.deepEqual(60, account.append(amount));
+            assert.equal(60, account.append(amount));
 
             //no change if negative amount
             let negativeAmount = -10;
             let account2 = new BankAccount(balance);
-            assert.deepEqual(10, account2.append(negativeAmount));
+            assert.equal(10, account2.append(negativeAmount));
         })
     })
 
@@ -32,12 +32,12 @@ describe('BankAccount', () => {
             let balance = 20;
             let amount = 10;
             let account = new BankAccount(balance);
-            assert.deepEqual(10, account.substract(amount));
+            assert.equal(10, account.substract(amount));
 
             //no change if negative amount
             let negativeAmount = -10;
             let account2 = new BankAccount(balance);
-            assert.deepEqual(20, account2.substract(negativeAmount));
+            assert.equal(20, account2.substract(negativeAmount));
         })
     })
 
@@ -51,13 +51,30 @@ describe('BankAccount', () => {
             let account2 = new BankAccount(balance);
             account2.append(10);
 
+            //account 3
+            let account3 = new BankAccount(balance);
+            account3.substract(30);
+
+            //account 4
+            let account4 = new BankAccount(balance);
+
+
             //merged history
             let history = [{operation: 'create', amount: 20}, {operation: 'create', amount: 20}, {operation: 'append', amount: 10}];
             account.merge(account2);
+
+            let history2 = [{operation: 'create', amount: 20}, {operation: 'create', amount: 20}, {operation: 'substract', amount: 30}];
+            account4.merge(account3);
+
             
-            //When 
+            //When the merge adds to the balance
             assert.deepEqual(history, account.history);
-            assert.deepEqual(50, account.current());
+            assert.equal(50, account.current());
+
+            //When the merge substracts from the balance
+            assert.deepEqual(history2, account4.history);
+            assert.equal(10, account4.current());
+
         })
     })
 
